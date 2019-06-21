044b2254d6384ad2f6d3337f74a350a03f2210b6
Alexander Kargl <alexanderkargl@gmail.com>
2019-06-21 13:52:53 +0200

    Bot Uptime Monitor

---
+++ config.js
@@ -0,0 +1,11 @@
+require("dotenv").config({silent: true});
+
+module.exports = {
+    DISCORD_TOKEN: process.env.DISCORD_TOKEN,
+    PREFIX: "?",
+
+    defaultChannels: {
+        569945613396082718: 570391301191761972,
+        337519743345098763: 337519743345098763
+    },
+}
