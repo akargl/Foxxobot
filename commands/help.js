5ede4743ad0e6b4f210f6795a76dfd2bf831b8e1
akargl <mail@akargl.com>
2020-07-11 19:33:50 +0200

    Fix typos

--- commands/help.js
+++ commands/help.js
@@ -8,10 +8,10 @@ module.exports = {
 	cooldown: 5,
 	execute(message, args) {
         const data = [];
-        const { commands} = message.client;
+        const {commands} = message.client;
 
         if (!args.length) {
-            data.push("Here\"s a list of all my commands:");
+            data.push("Here's a list of all my commands:");
             data.push(commands.map(command => command.name).join(", "));
             data.push(`\nYou can send \`${config.PREFIX}help [command name]\` to get info on a specific command!`);
 
