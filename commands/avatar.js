d023093fdac08da7b873a4d57899bed04c76da19
akargl <mail@akargl.com>
2022-01-21 19:24:18 +0100

    Show avatar in max size

--- commands/avatar.js
+++ commands/avatar.js
@@ -5,11 +5,11 @@ module.exports = {
 	usage: "[avatar <user>]",
 	execute(message, _args) {
 		if (!message.mentions.users.size) {
-			return message.channel.send(`Your avatar: ${message.author.displayAvatarURL({ format: "png", dynamic: true })}`);
+			return message.channel.send(`Your avatar: ${message.author.displayAvatarURL({ format: "png", dynamic: true, size: 4096 })}`);
 		}
 
 		const avatarList = message.mentions.users.map(user => {
-			return `${user.username}'s avatar: ${user.displayAvatarURL({ format: "png", dynamic: true })}`;
+			return `${user.username}'s avatar: ${user.displayAvatarURL({ format: "png", dynamic: true, size: 4096 })}`;
 		});
 
 		message.channel.send(avatarList.join("\n"));
